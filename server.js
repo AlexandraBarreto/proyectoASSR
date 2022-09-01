fffc0a04c722b8ebd039deac7bd270bbec4bbcb8
AlexandraBarreto <67971938+AlexandraBarreto@users.noreply.github.com>
2022-09-01 00:00:43 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+
+
+const app = express();
+const server = http.createServer(app);
+
+//creando socket y listening
+
+const io = require('socket.io')(server);
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname+'/public')));
+//export default app;
+
+
+io.on('connection', function(socket){
+    socket.on('newuser', function(username){
+        socket.broadcast.emit('update', username + 'joined the conversation');
+    });
+    socket.on('exituser', function(username){
+        socket.broadcast.emit('update', username + 'left the conversation');
+    });
+    socket.on('chat', function(message){
+        socket.broadcast.emit('chat', message);
+    });
+});
+//server.listen(3000);
+server.listen(app.get('port'));
+console.log(`server on port ${app.get('port')}`);
